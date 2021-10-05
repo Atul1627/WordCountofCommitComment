0d2e0af8147f4a345c4e5949958fbfc86100b690
Atul More <atulmore27@gmail.com>
2021-10-05 22:52:49 +0530

    Added Unit Testing sample.

---
+++ WordCount/UnitTestWordCount/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestWordCount")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestWordCount")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56d4bdc1-d982-4a2a-978e-26a3a316dfac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WordCount/UnitTestWordCount/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using WordCount.Models.CommentWordCount;
+
+namespace UnitTestWordCount
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string userName = "ATUL1627";
+            string token = "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";
+            string repoURL = "https://github.com/Atul1627/WordCountofCommitComment";
+            CommentWordCountModelEngine _model = new CommentWordCountModelEngine();
+
+            //Act
+            var result = _model.GetWordCount(userName, token, repoURL);
+
+            //Assert
+            Assert.AreEqual(18, result.SortedWordCount.Count);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            string userName = "ATUL1627";
+            string token = "btrnrnyjtybyewtwreetew tet";
+            string repoURL = "https://github.com/Atul1627/WordCountofCommitComment";
+            CommentWordCountModelEngine _model = new CommentWordCountModelEngine();
+
+            //Act
+            var result = _model.GetWordCount(userName, token, repoURL);
+
+            //Assert
+            Assert.AreEqual(null, result.AllAVLTrees);
+        }
+
+    }
+}
